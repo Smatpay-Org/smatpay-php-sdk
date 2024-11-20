68ce7c05db087103629852f256ffd1d86468a9c2
Anesu Paul Ngirande <anesupaulngirande@gmail.com>
2024-11-20 11:04:27 +0200

    change prod url and add fetch currencies

--- src/Constants/SmatpayURL.php
+++ src/Constants/SmatpayURL.php
@@ -6,21 +6,23 @@ class SmatpayURL
 {
     const SANDBOX_PAYMENT_URL = 'https://dev.smatpay.africa:8443/sandbox/init/authenticate/merchant/wallet';
 
-    const PROD_PAYMENT_URL = 'https://smatpay.africa:8443/sandbox/init/authenticate/merchant/wallet';
+    const PROD_PAYMENT_URL = 'https://live.smatpay.africa/sandbox/init/authenticate/merchant/wallet';
 
     const SANDBOX_ENQUIRE_URL = 'https://dev.smatpay.africa:8443/sandbox/api/getPaymentDetails';
 
-    const PROD_ENQUIRE_URL = 'https://smatpay.africa:8443/sandbox/api/getPaymentDetails';
+    const PROD_ENQUIRE_URL = 'https://live.smatpay.africa/sandbox/api/getPaymentDetails';
 
     const SANDBOX_AUTH_URL = 'https://dev.smatpay.africa:8443/auth/authenticate/sandbox/merchant';
 
-    const PROD_AUTH_URL = 'https://smatpay.africa:8443/auth/authenticate/sandbox/merchant';
+    const PROD_AUTH_URL = 'https://live.smatpay.africa/auth/authenticate/sandbox/merchant';
 
     const SANDBOX_BULK_PAYMENT_URL = "https://dev.smatpay.africa:8443/sandbox/init/authenticate/merchant/bulkWallet";
 
-    const PROD_BULK_PAYMENT_URL = "https://smatpay.africa:8443/init/authenticate/merchant/bulkWallet";
+    const PROD_BULK_PAYMENT_URL = "https://live.smatpay.africa/init/authenticate/merchant/bulkWallet";
 
     const SANDBOX_FAST_CHECKOUT = "https://dev.smatpay.africa:8443/api/merchant/sandbox/fastCheckOut";
 
-    const PROD_FAST_CHECKOUT = "https://smatpay.africa:8443/api/merchant/fastCheckOut";
+    const PROD_FAST_CHECKOUT = "https://live.smatpay.africa/api/merchant/fastCheckOut";
+
+    const PROD_CURRENCIES_URL = "https://live.smatpay.africa/init/currencies/all";
 }
---
+++ src/Gateway/Currency.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Smatpay\Gateway;
+
+use Smatpay\Constants\SmatpayURL;
+
+class Currency
+{
+    public function all(bool $isSandbox)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => SmatpayURL::PROD_CURRENCIES_URL,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        return json_decode($response);
+    }
+}
