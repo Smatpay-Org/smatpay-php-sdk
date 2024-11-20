68c325cf4eb1d27e48b4eff6ec3f5603a542b2a1
Anesu Paul Ngirande <anesupaulngirande@gmail.com>
2024-11-20 09:49:44 +0200

    add fast checkout

--- src/Constants/SmatpayURL.php
+++ src/Constants/SmatpayURL.php
@@ -19,4 +19,8 @@ class SmatpayURL
     const SANDBOX_BULK_PAYMENT_URL = "https://dev.smatpay.africa:8443/sandbox/init/authenticate/merchant/bulkWallet";
 
     const PROD_BULK_PAYMENT_URL = "https://smatpay.africa:8443/init/authenticate/merchant/bulkWallet";
+
+    const SANDBOX_FAST_CHECKOUT = "https://dev.smatpay.africa:8443/api/merchant/sandbox/fastCheckOut";
+
+    const PROD_FAST_CHECKOUT = "https://smatpay.africa:8443/api/merchant/fastCheckOut";
 }
