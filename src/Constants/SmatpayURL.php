c795d95717d1de98771d7470cdd5899fc0f56545
Anesu Ngirande <anesupaulngirande@gmail.com>
2025-09-09 11:07:33 +0200

    change dev url tp payment-sandbox

--- src/Constants/SmatpayURL.php
+++ src/Constants/SmatpayURL.php
@@ -4,23 +4,23 @@ namespace Smatpay\Constants;
 
 class SmatpayURL
 {
-    const SANDBOX_PAYMENT_URL = 'https://dev.smatpay.africa:8443/sandbox/init/authenticate/merchant/wallet';
+    const SANDBOX_PAYMENT_URL = 'https://payment-sandbox.smatpay.africa/sandbox/init/authenticate/merchant/wallet';
 
     const PROD_PAYMENT_URL = 'https://live.smatpay.africa/init/authenticate/merchant/wallet';
 
-    const SANDBOX_ENQUIRE_URL = 'https://dev.smatpay.africa:8443/sandbox/api/getPaymentDetails';
+    const SANDBOX_ENQUIRE_URL = 'https://payment-sandbox.smatpay.africa/sandbox/api/getPaymentDetails';
 
     const PROD_ENQUIRE_URL = 'https://live.smatpay.africa/api/getPaymentDetails';
 
-    const SANDBOX_AUTH_URL = 'https://dev.smatpay.africa:8443/auth/authenticate/sandbox/merchant';
+    const SANDBOX_AUTH_URL = 'https://payment-sandbox.smatpay.africa/auth/authenticate/sandbox/merchant';
 
     const PROD_AUTH_URL = 'https://live.smatpay.africa/auth/authenticate/merchant';
 
-    const SANDBOX_BULK_PAYMENT_URL = "https://dev.smatpay.africa:8443/sandbox/init/authenticate/merchant/bulkWallet";
+    const SANDBOX_BULK_PAYMENT_URL = "https://payment-sandbox.smatpay.africa/sandbox/init/authenticate/merchant/bulkWallet";
 
     const PROD_BULK_PAYMENT_URL = "https://live.smatpay.africa/init/authenticate/merchant/bulkWallet";
 
-    const SANDBOX_FAST_CHECKOUT = "https://dev.smatpay.africa:8443/api/merchant/sandbox/fastCheckOut";
+    const SANDBOX_FAST_CHECKOUT = "https://payment-sandbox.smatpay.africa/api/merchant/sandbox/fastCheckOut";
 
     const PROD_FAST_CHECKOUT = "https://live.smatpay.africa/api/merchant/fastCheckOut";
 
